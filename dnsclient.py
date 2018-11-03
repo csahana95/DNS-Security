e52c9c50aa2e1b7c000da59b86c832a86d1279aa
Sahana C <csahana95@gmail.com>
2018-11-03 12:55:27 -0400

    update

--- dnsclient.py
+++ dnsclient.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import time
 #hostname, sld, tld, port = 'www', 'integralist', 'co.uk', 80
 #target = '{}.{}.{}'.format(hostname, sld, tld)
 servername = sys.argv[1]
@@ -9,11 +10,13 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # connect the client
 # client.connect((target, port))
 client.connect(('0.0.0.0', 9999))
-
+t0= time.time()
 # send some data (in this case a HTTP GET request)
 client.send(servername)
 
 # receive the response data (4096 is recommended buffer size)
 response = client.recv(4096)
+t1 = time.time()
 
 print response
+print "Time taken: {0}".format(t1-t0)
