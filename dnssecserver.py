e0679951b35b050f263b7af83a0affdc0c592c31
shweta1609 <shwetaa.singhal@gmail.com>
2018-11-21 19:25:17 -0500

    intermediate changes

--- dnssecserver.py
+++ dnssecserver.py
@@ -1,17 +1,17 @@
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 from Crypto import Random
-import dnsserver
+
 # from Crypto import Signature
 
 random_generator = Random.new().read
 key = RSA.generate(1024, random_generator)
 
-public_key = key.publickey()
+DNS_KEY = key.publickey()
+
 text = 'sdfhifvn'
 hash = SHA256.new(text.encode('utf8')).digest()
 signature = key.sign(hash, '')
-
-
+RRSIG = signature
 print(signature)
-print(public_key.verify(hash, signature))
+print(DNS_KEY.verify(hash, signature))
